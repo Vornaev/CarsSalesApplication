5aef6e230df92ecb6a534e3476af5a50fe23240f
Viktor <vice.eu@gmail.com>
2016-10-24 20:01:02 +0200

    #initial

---
+++ app/src/main/java/com/apps/viktor/oglasiapp/Views/Base/BaseFragment.java
@@ -0,0 +1,13 @@
+package com.apps.viktor.oglasiapp.Views.Base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Viktor on 24-Oct-16.
+ */
+
+public abstract class BaseFragment extends Fragment {
+
+
+
+}
---
+++ app/src/main/java/com/apps/viktor/oglasiapp/Views/Base/IFragment.java
@@ -0,0 +1,8 @@
+package com.apps.viktor.oglasiapp.Views.Base;
+
+/**
+ * Created by Viktor on 24-Oct-16.
+ */
+
+public interface IFragment {
+}
